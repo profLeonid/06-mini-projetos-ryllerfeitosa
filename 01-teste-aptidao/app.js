53373bc52e153f41ae6ad446637b38e895124912
RILLER DA COSTA FEITOSA <25203652@CFP127ALUNO.LOCAL>
2026-03-10 11:27:44 -0300

    Em andamento

---
+++ 01-teste-aptidao/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const avaliar = function(){
+    const tempo     = document.getElementById('tempo')
+    const resultado = document.getElementById('resultado')
+    removerClasses()
+    if(tempo.value < 14){
+        resultado.textContent = 'Apto'
+        resultado.classList.add('apto')
+    }else{
+        resultado.textContent = 'Não apto'
+        resultado.classList.add('naoApto')
+    }
+}
+
+const removerClasses = function(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('Apto', 'naoApto')
+}
---
+++ 02-calculadora-desconto/app.js
@@ -0,0 +1,6 @@
+const calcularDesconto = function() {
+    const precoOriginal = document.getElementById('precoOriginal')
+    const desconto      = document.getElementById('desconto')
+
+    const valorFinal = precoOriginal.value - desconto.value
+}
